8d7ffac49da582ec19c74768d6e81a97c737dc76
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-17 21:09:34 +0530

    Add files via upload

---
+++ 01_data_retrieval/solutions.sql
@@ -0,0 +1,5 @@
+SELECT * FROM students;
+SELECT name,age FROM students;
+SELECT * FROM students WHERE age = 21;
+SELECT * FROM students WHERE city = 'Delhi';
+SELECT * FROM students WHERE grade = 'A';
