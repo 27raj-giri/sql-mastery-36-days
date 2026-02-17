f098e24023bcd6256fa50f556bfb393f71a7b540
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-17 21:09:10 +0530

    Add files via upload

---
+++ 01_data_retrieval/questions.sql
@@ -0,0 +1,7 @@
+-- Day 01 Questions
+
+-- Q1: Select all columns from students table
+-- Q2: Select only name and age columns
+-- Q3: Select students who are 21 years old
+-- Q4: Select students from Delhi
+-- Q5: Select students with grade 'A'
