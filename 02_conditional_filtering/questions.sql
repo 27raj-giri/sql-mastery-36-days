22cee1b3c9569cfc59178aea53d651e504576945
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-18 20:59:17 +0530

    Add files via upload

---
+++ 02_conditional_filtering/questions.sql
@@ -0,0 +1,7 @@
+-- Day 02 Questions
+
+-- Q1: Select students older than 21
+-- Q2: Select students with marks greater than or equal to 80
+-- Q3: Select students who are NOT from Delhi
+-- Q4: Select students with age between 20 and 22 (inclusive)
+-- Q5: Select students from Mumbai OR Pune
