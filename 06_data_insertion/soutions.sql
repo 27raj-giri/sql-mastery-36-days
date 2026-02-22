d26ee4107e4b02cfdecb15291762fca1ceb9c9ba
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-22 14:48:05 +0530

    Add files via upload

---
+++ 06_data_insertion/soutions.sql
@@ -0,0 +1,27 @@
+--1
+INSERT INTO students
+VALUES (9, 'Rohan', 23, 'B', 'Jaipur', 78);
+
+--2
+INSERT INTO products
+VALUES (9, 'Headphones', 'Electronics', 2000, 35, 'Chennai');
+
+--3
+INSERT INTO students
+VALUES
+(10, 'Simran', 21, 'A', 'Lucknow', 86),
+(11, 'Arjun', 22, 'C', 'Indore', 68);
+
+--4
+INSERT INTO products (id, name, price)
+VALUES(10, 'Notebook', 50);
+
+--5
+INSERT INTO products
+VALUES
+(11, 'Tablet', 'Electronics', 15000, 20, 'Delhi'),
+(12, 'Chair Mat', 'Furniture', 1200, 15, 'Mumbai'),
+(13, 'USB Hub', 'Electronics', 800, 40, 'Bangalore');
+
+SELECT * FROM students
+SELECT * FROM products
