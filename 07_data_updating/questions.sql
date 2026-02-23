0ad899dfbb55f68b93ad18944e4eb7ea94d66468
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-23 19:54:01 +0530

    Add files via upload

---
+++ 07_data_updating/questions.sql
@@ -0,0 +1,11 @@
+-- Day 07 Questions
+
+-- Q1: Update Aayush's marks to 87
+
+-- Q2: Increase price of all Electronics products by 10%
+
+-- Q3: Change grade to 'B' for all students with marks between 70 and 79
+
+-- Q4: Update stock to 0 for products priced below 1000
+
+-- Q5: Change city to 'Hyderabad' for student with id 4
