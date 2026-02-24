da963b4c4bef994063a9e5400e3744ccece18abf
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-24 17:56:48 +0530

    Add files via upload

---
+++ 08_data_deletion/questions.sql
@@ -0,0 +1,11 @@
+-- Day 08 Questions
+
+-- Q1: Delete student with id 8
+
+-- Q2: Delete all products with stock less than 20
+
+-- Q3: Delete students from Delhi with marks < 70
+
+-- Q4: Delete products in Furniture category priced above 5000
+
+-- Q5: Delete all students with grade 'C'
