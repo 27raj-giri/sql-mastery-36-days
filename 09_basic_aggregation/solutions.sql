7b370c4045f590e8d4dab6c23235d8d93f55beed
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-25 23:29:07 +0530

    Add files via upload

---
+++ 09_basic_aggregation/solutions.sql
@@ -0,0 +1,19 @@
+--1
+SELECT COUNT(*)
+FROM orders;
+
+--2
+SELECT SUM(amount)
+FROM orders;
+
+--3
+SELECT AVG(amount)
+FROM orders;
+
+--4
+SELECT MIN(amount)
+FROM orders;
+
+--5
+SELECT MAX(quantity)
+FROM orders;
