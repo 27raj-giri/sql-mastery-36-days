e6796d38d30a16b8f281c200e3ebfd598b75eab2
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-25 23:28:16 +0530

    Add files via upload

---
+++ 09_basic_aggregation/questions.sql
@@ -0,0 +1,11 @@
+-- Day 09 Questions
+
+-- Q1: Count total number of orders
+
+-- Q2: Find sum of all order amounts
+
+-- Q3: Calculate average order amount
+
+-- Q4: Find minimum order amount
+
+-- Q5: Find maximum quantity in any single order
