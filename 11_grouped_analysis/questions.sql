877f0322cb1894d791290c758aa7685414924c89
Aayush Raj Giri <heyayush27@gmail.com>
2026-02-27 21:26:41 +0530

    Add files via upload

---
+++ 11_grouped_analysis/questions.sql
@@ -0,0 +1,11 @@
+-- Day 11 Questions
+
+-- Q1: Count number of orders per customer
+
+-- Q2: Calculate total amount spent by each customer
+
+-- Q3: Find total quantity ordered for each category
+
+-- Q4: Count orders from each city
+
+-- Q5: Calculate average order amount by category
