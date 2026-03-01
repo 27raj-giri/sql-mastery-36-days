6070c45cac620fed71339346b5e05ab88940fa57
Aayush Raj Giri <heyayush27@gmail.com>
2026-03-01 19:20:31 +0530

    Add files via upload

---
+++ 13_inner_join_basics/questions.sql
@@ -0,0 +1,11 @@
+-- Day 13 Questions
+
+-- Q1: Show customer names with their order amounts
+
+-- Q2: Show customer cities with order dates
+
+-- Q3: Show all order details with customer emails
+
+-- Q4: Show customer names and order status
+
+-- Q5: Show orders with customer names where amount > 500
