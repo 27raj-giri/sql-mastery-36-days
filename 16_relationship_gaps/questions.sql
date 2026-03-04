9892bb2b0fd75183251dd5e78086c16f71860c26
Aayush Raj Giri <heyayush27@gmail.com>
2026-03-04 21:22:39 +0530

    Add files via upload

---
+++ 16_relationship_gaps/questions.sql
@@ -0,0 +1,11 @@
+-- Day 16 Questions
+
+-- Q1: Show all orders including those with invalid customer_ids
+
+-- Q2: Find orders that don't have a matching customer
+
+-- Q3: Show customers from Delhi with their completed orders only
+
+-- Q4: Show customers who placed orders worth more than 1000 in total
+
+-- Q5: Show customers and count of their cancelled orders (including customers with 0 cancellations)
